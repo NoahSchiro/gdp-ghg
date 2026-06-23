96ef6dc0774cc5c13588f132da6a52c776aa8b5f
Noah Schiro <noahschiro@rocketmail.com>
2026-06-23 19:19:08 -0400

    Add imagery

--- README.md
+++ README.md
@@ -1,5 +1,30 @@
 # Does GDP correlate with GHG emissions?
 
-Sources:
+List available countries:
+```
+uv run main.py list
+```
+
+Plot a given country (optionally use log scale and optionally save it to a file):
+```
+uv run main.py plot -c <country> [--log] [--save file_path]
+```
+
+Sources for the data:
 [GDP](https://data.imf.org/en/Data-Explorer?datasetUrn=IMF.RES:WEO(9.0.0))
 [Emissions](https://edgar.jrc.ec.europa.eu/report_2024?vis=co2tot#data_download)
+
+## Gallery
+---
+
+### China
+![](images/china.png)
+![](images/china_log.png)
+
+### United States
+![](images/united_states.png)
+![](images/united_states_log.png)
+
+### India
+![](images/india.png)
+![](images/india_log.png)
--- main.py
+++ main.py
@@ -81,6 +81,38 @@ def ghg():
     )
     return df
 
+def cmd_list(df):
+    for c in df["country"].unique():
+        print(c)
+
+def cmd_plot(args, df):
+    # Make sure everything is sorted, and then normalize the values based on 1980 values
+    df["gdp"] = df.groupby("country")["gdp"].transform(lambda x: x / x.iloc[0] * 100)
+    df["ghg"] = df.groupby("country")["ghg"].transform(lambda x: x / x.iloc[0] * 100)
+    
+    one_country = df[df["country"] == args.country].sort_values("year")
+    one_country["year"] = one_country["year"].astype(int)
+    slope, _ = np.polyfit(np.log(one_country["gdp"]), np.log(one_country["ghg"]), 1)
+
+    plt.style.use("seaborn-v0_8-darkgrid")
+
+    ax = one_country.plot(x="year", y=["gdp", "ghg"], logy=args.log)
+    log_title = "(log)" if args.log else ""
+    ax.set_title(f"{args.country} GDP growth vs. GHG growth {log_title}")
+    ax.text(0.05, 0.95,
+        f"Slope = {slope:.3f}",
+        transform=ax.transAxes,
+        va="top",
+        bbox=dict(facecolor="white", alpha=0.8, edgecolor="none")
+    )
+    ax.set_xticks(np.arange(1980, 2024, 5))
+    plt.legend(["GDP (% of 1980)", "GHG (% of 1980)"])
+
+    if args.save: 
+        plt.savefig(args.save)
+    else:
+        plt.show()
+
 if __name__=="__main__":
     parser = ArgumentParser()
     subparsers = parser.add_subparsers(dest="command", required=True)
@@ -88,6 +120,7 @@ if __name__=="__main__":
     plot_parser = subparsers.add_parser("plot")
     plot_parser.add_argument("--country", "-c", required=True)
     plot_parser.add_argument("--log", action="store_true")
+    plot_parser.add_argument("--save", type=str) # path to save location
 
     list_parser = subparsers.add_parser("list")
     args = parser.parse_args()
@@ -103,18 +136,7 @@ if __name__=="__main__":
     df = df.sort_values(["country", "year"])
 
     if args.command == "list":
-        for c in df["country"].unique():
-            print(c)
+        cmd_list(df)
     else:
-        # Make sure everything is sorted, and then normalize the values based on 1980 values
-        df["gdp"] = df.groupby("country")["gdp"].transform(lambda x: x / x.iloc[0] * 100)
-        df["ghg"] = df.groupby("country")["ghg"].transform(lambda x: x / x.iloc[0] * 100)
-        
-        one_country = df[df["country"] == args.country].sort_values("year")
-        slope, _ = np.polyfit(np.log(one_country["gdp"]), np.log(one_country["ghg"]), 1)
-        r2 = one_country[["gdp", "ghg"]].corr().iloc[0, 1] ** 2
-        ax = one_country.plot(x="year", y=["gdp", "ghg"], logy=args.log)
-        plt.legend(["GDP", "GHG"])
-        ax.text(0.05, 0.95, f"Slope = {slope:.3f}", transform=ax.transAxes, va="top")
-        plt.show()
+        cmd_plot(args, df)
 
